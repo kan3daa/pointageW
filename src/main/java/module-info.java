ac9d878b54fd30c612489586aa5ba6f34a9f35d3
kan3daa <matias.sanchesdasilva@gmail.com>
2026-01-08 15:12:54 +0100

    feat: initialisation de l’architecture du projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.pointagew {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.pointagew to javafx.fxml;
+    exports com.example.pointagew;
+}
